925b1855e28599040519832c53bec1dacfecafd0
sluckhurst <jalaconsulting@gmail.com>
2025-11-19 11:14:22 +0000

    Commit 3.0

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Niall"));
+        System.out.println("Veersion 3 Release");
         System.out.println(sayGoodbye());
     }
 
